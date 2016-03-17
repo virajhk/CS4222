ba84eb15ae4ce44007609279fc5dbfebc98003f2
Viraj <virajhk@gmail.com>
2016-03-17 13:26:06 +0800

    Project Base Code

---
+++ SensorLogApp/app/src/main/java/com/cs4222/khuthia/sensorlogapp/UserActivities.java
@@ -0,0 +1,10 @@
+package com.cs4222.khuthia.sensorlogapp;
+
+/**
+   Enumeration of all user activities.
+
+   @note  Simply add any activities here, and it will automatically appear in the GUI.
+ */
+public enum UserActivities {
+    Confirm , IDLE_INDOOR , IDLE_OUTDOOR , WALKING , BUS , TRAIN , CAR , JOGGING, OTHER , INCORRECT
+}
